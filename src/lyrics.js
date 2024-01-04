f4de8865d170453d0ff48ce31a183e9766d3861e
Photon 8617 <74341381+shubham01015@users.noreply.github.com>
2024-01-04 15:22:05 +0530

    Update lyrics Format

--- src/lyrics.js
+++ src/lyrics.js
@@ -1,26 +1,56 @@
+function fetchLyrics(artist, songTitle, videoChannel) {
+  var apiUrl = `https://api.lyrics.ovh/v1/${artist}/${songTitle}`;
 
-function fetchLyrics(artist, songTitle) {
-    var apiUrl = `https://api.lyrics.ovh/v1/${artist}/${songTitle}`;
-    console.log(apiUrl); 
-    fetch(apiUrl)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok.');
-        }
-        return response.json();
-      })
-      .then(data => {
-        if (data.lyrics) {
-          displayLyrics(data.lyrics);
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.lyrics) {
+        displayLyrics(data.lyrics);
+      } else {
+        if (videoChannel) {
+          fetchLyrics(videoChannel, songTitle); // Try fetching lyrics using video channel as artist
         } else {
           displayLyrics('Lyrics not found');
         }
-      })
-      .catch(error => {
-        console.error('Error fetching lyrics:', error);
-        displayLyrics('Error fetching lyrics');
-      });
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching lyrics:', error);
+      displayLyrics('Error fetching lyrics');
+    });
+}
+
+function updateVideoTitle2() {
+  var videoData = player.getVideoData();
+  var videoTitle = videoData.title;
+  var videoChannel = videoData.author;
+  var channelWords = videoChannel.split(' ');
+  if (channelWords.length > 2) {
+   videoChannel = channelWords.slice(0, -2).join(' ');
+ }
+
+  var splitTitle = videoTitle.split(' - ');
+  if (splitTitle.length === 2) {
+    var artist = splitTitle[0];
+    var songTitle = splitTitle[1];
+    fetchLyrics(artist, songTitle);
+  } else {
+    // If title doesn't follow the specified format, try fetching lyrics using video channel as artist
+    fetchLyrics(videoChannel, videoTitle, videoChannel);
   }
+
+  
+
+  var updatedTitle = `${videoTitle} (${videoChannel})`;
+  var videoTitleElement = document.querySelector('.video-title2');
+  videoTitleElement.textContent = updatedTitle;
+}
+
  
   function displayLyrics(lyrics) { console.log(lyrics); 
     var lyricsContainer = document.getElementById('lyrics-container');
@@ -30,22 +60,48 @@ function fetchLyrics(artist, songTitle) {
       lyricsContainer.innerHTML = formattedLyrics; // Update the content of the lyrics container
     }
   }
-  function updateVideoTitle2() {
-    var videoData = player.getVideoData();
-    var videoTitle = videoData.title;
+  // function updateVideoTitle2() {
+  //   var videoData = player.getVideoData();
+  //   var videoTitle = videoData.title;
+  //   var videoChannel = videoData.author;
   
-    var splitTitle = videoTitle.split(' - ');
-    if (splitTitle.length === 2) {
-      var artist = splitTitle[0];
-      var songTitle = splitTitle[1];
-      fetchLyrics(artist, songTitle);
-    } else {
-      displayLyrics('Invalid title format');
-    }
+  //   var splitTitle = videoTitle.split(' - ');
+  //   if (splitTitle.length === 2) {
+  //     var artist = splitTitle[0];
+  //     var songTitle = splitTitle[1];
+  //     fetchLyrics(artist, songTitle);
+  //   } else {
+  //     displayLyrics('Invalid title format');
+  //   }
   
-    var videoTitleElement = document.querySelector('.video-title2');
-    videoTitleElement.textContent = videoTitle;
-  }
+  //   // Removing the last two words if videoChannel has more than two words
+  //   var channelWords = videoChannel.split(' ');
+  //   if (channelWords.length > 2) {
+  //     videoChannel = channelWords.slice(0, -2).join(' ');
+  //   }
+  
+  //   var updatedTitle = `${videoTitle} (${videoChannel})`;
+  //   var videoTitleElement = document.querySelector('.video-title2');
+  //   videoTitleElement.textContent = updatedTitle;
+  // }
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   function toggleLyrics() {
     var lyricsContainer = document.getElementById('lyrics-container');
     if (lyricsContainer) {
@@ -66,4 +122,21 @@ function fetchLyrics(artist, songTitle) {
       }
     }
   }
+  // function updateVideoTitle2() {
+  //   var videoData = player.getVideoData();
+  //   var videoTitle = videoData.title;
+  //   var videoAuthor = videoData.author;
   
+  //   var splitTitle = videoTitle.split(' - ');
+  //   if (splitTitle.length === 2) {
+  //     var artist = splitTitle[0];
+  //     var songTitle = splitTitle[1];
+  //     fetchLyrics(artist, songTitle);
+  //   } else {
+  //     displayLyrics('Invalid title format');
+  //   }
+  
+  //   var videoInfo = videoTitle + ' - ' + videoAuthor;
+  // var videoTitleElement = document.querySelector('.video-title2');
+  // videoTitleElement.textContent = videoInfo;
+  // }
